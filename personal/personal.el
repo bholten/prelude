ee2c63afd09b675051a39e857b9a0bd2945b86d7
Brennan Holten <brennan.holten@47deg.com>
2022-01-27 07:29:44 -0600

    Add Docker, Haskell LSP

--- personal/personal.el
+++ personal/personal.el
@@ -5,6 +5,7 @@
 (prelude-require-package 'zig-mode)
 (prelude-require-package 'lsp-mode)
 (prelude-require-package 'ccls)
+(prelude-require-package 'docker)
 
 (setq ccls-executable "/usr/local/bin/ccls")
 
@@ -12,3 +13,8 @@
   (guru-mode -1))
 
 (add-hook 'prelude-prog-mode-hook 'disable-guru-mode t)
+
+(add-hook 'haskell-mode-hook #'lsp)
+(add-hook 'haskell-literate-mode-hook #'lsp)
+
+(setq lsp-haskell-server-path "$HOME/.ghcup/bin/haskell-language-server-wrapper")
